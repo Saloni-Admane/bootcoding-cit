e10183433b69402b33104f71d8e34cbd661f9798
Sankalppawar28 <sankalppawar2003@gmail.com>
2023-08-18 14:13:18 +0530

    same number

---
+++ src/test/java/com/bootcoding/InputEx.java
@@ -0,0 +1,19 @@
+package com.bootcoding;
+
+import java.util.Scanner;
+
+public class InputEx {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter First Number:");
+        int x = sc.nextInt();
+        System.out.println("Enter Second Number:");
+        int y = sc.nextInt();
+        if(x == y) {
+            System.out.println("Number are same!");
+        }else{
+            System.out.println("Number are different!");
+        }
+    }
+}
